22b0ee83680f8edb0d5913220408e39e494d79cd
kubra <kajalbalajipeta786@gmail.com>
2026-09-14 12:23:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
+page = st.sidebar.selectbox(
+    "Choose a page",
+    ["🏠 Home", "📚 Notes", "❓ Quiz"]
+)
+
+if page == "🏠 Home":
+    st.title("Home")
+
 elif page == "📚 Notes":
-
     st.title("📚 Study Notes")
-
     topic = st.text_input("Enter Topic")
-
     if st.button("Generate Notes"):
         if topic:
             st.markdown(generate_notes(topic))
         else:
             st.warning("Please enter a topic.")
+
+elif page == "❓ Quiz":
+    st.title("Quiz")
